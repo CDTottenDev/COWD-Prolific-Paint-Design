42059b9e369629c65e35818f944e4abc545c5ec6
tottbob <tottencolt45@gmail.com>
2025-05-18 23:11:43 -0400

    favicon icon update

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -72,7 +72,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/webp" href="/paint-splash-mobile-optimized-360x640.webp" />
+        <link rel="apple-touch-icon" href="/paint-splash-mobile-optimized-360x640.webp" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#ffffff" />
       </head>
